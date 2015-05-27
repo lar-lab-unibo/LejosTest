7369715cfce7839b779009557b7ecd085b810c7b
lar-lab-unibo <lar.lab.unibo@gmail.com>
2015-05-27 16:13:01 +0200

    Prova

--- src/lar/nxt/tests/EchoTest.java
+++ src/lar/nxt/tests/EchoTest.java
@@ -41,14 +41,40 @@ public class EchoTest {
 					LCD.clear();
 				}
 				if(command.name.equals(CommandName.POSES)){
-					Graphics g = new Graphics();
 					
+					//Inserisce nell'hashtable tutti i vettori Q
 					Hashtable<String , SbunTarget2D> table = (Hashtable<String , SbunTarget2D>)command.payload;
 					
-					SbunTarget2D ta = table.get("/lar_marker_201");
+					//Prende i vettori q in base al nome
+					SbunTarget2D robot = table.get("/lar_marker_202");
+					SbunTarget2D target = table.get("/lar_marker_200");
 					
 					
-					System.out.println(ta.x+","+ta.y+","+ta.angle);
+					double target_angle = target.angle * 180 / Math.PI;
+					double robot_angle = robot.angle * 180 / Math.PI;
+					if (robot_angle<0) robot_angle=360+robot_angle;
+					if (target_angle<0) target_angle=360+target_angle;
+					
+					//target_angle = target_angle % 360;
+					//robot_angle = robot_angle % 360;
+					double delta_angle = robot_angle-target_angle;
+					double dd = delta_angle;// Math.sin(delta_angle);
+					float k =0.8f;
+					
+					int l_power = (int)(dd*k);
+					int r_power = -(int)(dd*k);
+					
+					LCD.clear();
+					LCD.drawString("q_goal:"+target_angle, 0, 0);
+					LCD.drawString("q_robot:"+robot_angle, 0, 1);
+					LCD.drawString("diff:"+delta_angle, 0, 3);
+					
+					
+					
+					m_left.setPower(l_power);
+					m_right.setPower(r_power);
+					//System.out.println(str);
+					
 					
 					//m.setPower((int)ta.angle);
 					
