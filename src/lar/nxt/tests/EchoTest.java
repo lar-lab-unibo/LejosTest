97e3dc6ad896f5fb925d5717a12338b64326c0f7
lar-lab-unibo <lar.lab.unibo@gmail.com>
2015-06-03 10:06:24 +0200

    Private

--- src/lar/nxt/tests/EchoTest.java
+++ src/lar/nxt/tests/EchoTest.java
@@ -8,6 +8,7 @@ import lar.nxt.bluetooth.BluetoothSlaveNode;
 import lar.nxt.protocols.sbun.SbunCommand;
 import lar.nxt.protocols.sbun.SbunCommand.CommandName;
 import lar.nxt.protocols.sbun.SbunCommand.SbunTarget2D;
+import lar.nxt.unicycle.Donkey;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
@@ -29,8 +30,9 @@ public class EchoTest {
 		NXTMotor m_left = new NXTMotor(MotorPort.A);
 		NXTMotor m_right = new NXTMotor(MotorPort.C);
 		
-		 
+		Donkey donkey = new Donkey(Motor.A, Motor.C, 0.028, 0.115, 0.05);
 		
+		 
 		/**
 		 * CREATES INLINE BLUETOOTHNODE LISTENER
 		 */
@@ -56,73 +58,15 @@ public class EchoTest {
 					SbunTarget2D target = table.get("/lar_marker_200");
 					
 					SbunTarget2D time = table.get("time");
-					LCD.drawString("time:"+time.x, 0, 5);
-					/*
-					double r = 0.028;
-					double L = 0.05;
-					double d = 0.115;
-					Matrix D = new Matrix(2,2);
-					D.set(0, 0, r/2);
-					D.set(0, 1, r/2);
-					D.set(1, 0, r/d);
-					D.set(1, 1, -r/d);
+					LCD.drawString("theta:"+robot.angle*180/Math.PI, 0, 5);
 					
-					Matrix C = new Matrix(2,2);
-					C.set(0,0, Math.cos(robot.angle));
-					C.set(0,1, -L*Math.sin(robot.angle));
-					C.set(1,0, Math.sin(robot.angle));
-					C.set(1,1, L*Math.cos(robot.angle));
+					donkey.setGoal(target.x, target.y, target.angle);
+					donkey.updateOdometry(robot.x, robot.y, robot.angle);
 					
-					Matrix A = C.times(D);
-					
-					double K = 10;
-					double x_dot=-K*(robot.x-target.x);
-					double y_dot=-K*(robot.y-target.y);
-					
-					
-					//LCD.drawString("X:"+-x_dot, 0, 0);
-					//LCD.drawString("Y:"+-y_dot, 0, 1);
-					Matrix P_dot = new Matrix(2,1);
-					P_dot.set(0, 0, x_dot );
-					P_dot.set(1, 0, y_dot );
-					
-					Matrix omegas = A.inverse().times(P_dot);
-					
-					
-					double VK = 1;
-					
-					int w_r = (int)(VK*omegas.get(0, 0));
-					int w_l = (int)(VK*omegas.get(1, 0));
-					
-					
-					Motor.A.setSpeed(Math.abs(w_l));
-					Motor.C.setSpeed(Math.abs(w_r));//(int)(VK*omegas.get(1, 0)));
-					
-					
-					if(w_l>=0){
-						Motor.A.forward();
-					}else{
-						Motor.A.backward();
-					}
-					
-					if(w_r>=0){
-						Motor.C.forward();
-					}else{
-						Motor.C.backward();
-					}
-					*/
-					
-				
-					
-					//m.setPower((int)ta.angle);
 					
 					
 				}
-				if(command.name.equals(CommandName.MOVE)){
-					float[] vel = (float[])command.payload;
-					m_left.setPower((int)vel[0]);
-					m_right.setPower((int)vel[1]);
-				}
+			
 			}
 		});
 		
@@ -143,5 +87,6 @@ public class EchoTest {
 		
 		
 		bnode.forceClose();
+		donkey.mainThread.join();
 	}
 }
--- src/lar/nxt/unicycle/Donkey.java
+++ src/lar/nxt/unicycle/Donkey.java
@@ -1,5 +1,160 @@
 package lar.nxt.unicycle;
 
-public class Donkey {
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.Settings;
+import lejos.nxt.SystemSettings;
+import lejos.util.Matrix;
 
+public class Donkey implements Runnable {
+
+	protected double radius = 0.0;
+	protected double interaxis = 0.0;
+	protected double b_distance = 0.0;
+
+	protected Matrix D;
+	protected Matrix C;
+	protected Matrix A;
+
+	protected double theta = 0.0;
+	protected Matrix p_goal;
+	protected Matrix p;
+	protected Matrix p_dot;
+
+	protected Matrix omegas;
+
+	
+	public double K_OMEGA = 0.2* 180.0/Math.PI;
+	public double K_ATT = 20.0/K_OMEGA;
+
+	protected NXTRegulatedMotor left_motor;
+	protected NXTRegulatedMotor right_motor;
+
+	
+	public boolean alive = true;
+	public Thread mainThread;
+	
+	/**
+	 * Costruisce il Mulo
+	 * 
+	 * @param radius
+	 *            raggio ruota
+	 * @param interaxis
+	 *            distanza interasse
+	 * @param b_distance
+	 *            distanza punt B
+	 */
+	public Donkey(NXTRegulatedMotor left_motor, NXTRegulatedMotor right_motor,
+			double radius, double interaxis, double b_distance) {
+		
+		this.left_motor = left_motor;
+		this.right_motor = right_motor;
+		
+		this.radius = radius;
+		this.interaxis = interaxis;
+		this.b_distance = b_distance;
+
+		this.D = new Matrix(2, 2);
+		this.C = new Matrix(2, 2);
+
+		this.p = new Matrix(2, 1);
+		this.p_dot = new Matrix(2, 1);
+		this.p_goal = new Matrix(2, 1);
+		this.omegas = new Matrix(2, 1);
+
+		this.D.set(0, 0, this.radius / 2);
+		this.D.set(0, 1, this.radius / 2);
+		this.D.set(1, 0, this.radius / this.interaxis);
+		this.D.set(1, 1, -this.radius / this.interaxis);
+		
+		//Initialization
+		updateOdometry(0, 0, 0);
+		setGoal(0, 0, 0);
+		
+		mainThread = new Thread(this);
+		mainThread.start();
+	}
+
+	/**
+	 * Aggiorna matrice A in base al Theta del robot
+	 * 
+	 * @param theta
+	 */
+	public synchronized void updateOdometry(double x, double y, double theta) {
+		p.set(0, 0, x);
+		p.set(1, 0, y);
+		this.theta = theta;
+	}
+
+	/**
+	 * Setta la posizione del Goal
+	 * 
+	 * @param x
+	 * @param y
+	 * @param theta
+	 */
+	public synchronized void setGoal(double x, double y, double theta)  {
+		this.p_goal.set(0, 0, x);
+		this.p_goal.set(1, 0, y);
+	}
+
+	/**
+	 * Aggiorna lo stato del robot
+	 */
+	public void updateTime(){
+		long t1 = System.currentTimeMillis();
+		C.set(0, 0, Math.cos(theta));
+		C.set(0, 1, -this.b_distance * Math.sin(theta));
+		C.set(1, 0, Math.sin(theta));
+		C.set(1, 1, this.b_distance * Math.cos(theta));
+
+		A = C.times(D);
+		
+		p_dot = p.minus(p_goal).times(-K_ATT);
+		omegas = A.inverse().times(p_dot).times(K_OMEGA);
+		long t2 = System.currentTimeMillis();
+		LCD.drawString("ms:"+(t2-t1), 0, 6);
+	}
+
+	
+	/**
+	 * Applica le Omega ai motori con le dovute precauzioni di direzione
+	 */
+	public synchronized void actuatesMotors(){
+		int w_r = (int)omegas.get(0, 0);
+		int w_l = (int)omegas.get(1, 0);
+		
+		left_motor.setSpeed(w_l);
+		right_motor.setSpeed(w_r);
+		
+		if(w_l>=0){
+			left_motor.forward();
+		}else{
+			left_motor.backward();
+		}
+		
+		if(w_r>=0){
+			right_motor.forward();
+		}else{
+			right_motor.backward();
+		}
+	}
+
+	/**
+	 * Ciclo principale
+	 */
+	@Override
+	public void run() {
+		while(alive){
+			updateTime();
+			actuatesMotors();
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
